baca7195c0b8d59757aa5efce5ed6479c99d0cd8
chuthulhu <misterakichu@gmail.com>
2026-01-01 09:36:09 +0000

    feat: Configure search API route to use edge runtime

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
+export const runtime = 'edge';
+
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const isbn = searchParams.get('isbn');
